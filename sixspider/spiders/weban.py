692680dee9df50c7d91364bf42f0edba968e20fb
Yongyu Lee <aleeyyu+01@gmail.com>
2015-08-07 04:13:19 +0900

    add the start url for weban

--- sixspider/spiders/weban.py
+++ sixspider/spiders/weban.py
@@ -3,10 +3,10 @@ import scrapy
 
 
 class WebanSpider(scrapy.Spider):
-    name = "weban"
-    allowed_domains = ["weban.jp"]
+    name = 'weban'
+    allowed_domains = ['weban.jp']
     start_urls = (
-        'http://www.weban.jp/',
+        'http://weban.jp/webapp/gen/list/itemSearchList?CMD=300&V5=1&V1=05&FID=300&A1=03&AX=1&V24=1&Z1=014&J1=12&J1=16&J1=03&J1=06&J1=08&J1=05&J1=01&J1=10&J1=04&J1=07&J1=14&J1=11&J1=02&J1=13&J1=15&J1=09',
     )
 
     def parse(self, response):
