fb3fc99fa0f693b2db707df69106f547cdb62030
Yongyu Lee <aleeyyu+01@gmail.com>
2015-08-28 10:23:24 +0900

    add TODO

--- sixspider/spiders/weban.py
+++ sixspider/spiders/weban.py
@@ -11,11 +11,12 @@ class WebanSpider(scrapy.Spider):
         'http://weban.jp/webapp/gen/list/itemSearchList/'
         '?CMD=300&FID=300&A1=03&AX=1&V1=05&V2=1&V13=40&Z1=014&V3=50&V5=1&V24=0',
     )
-    # 200 milliseconds delay (0.5 * 0.2 < delay < 1.5 * 0.2)
-    download_delay = 0.2
+    # 400 milliseconds delay (0.5 * 0.4 < delay < 1.5 * 0.4)
+    download_delay = 0.4
     next_page_xpath = '//*[@id="pagerTopList"]//li[@class="paging_next"]/a/@href'
     detail_page_xpath = u'//*[@id="mainContents"]//a[img[@alt="詳細を見る"]]/@href'
 
+    # TODO: http://stackoverflow.com/questions/28640102/retrying-a-scrapy-request-even-when-receiving-a-200-status-code
     def parse(self, response):
         # find all detail pages
         for detail_page_url in response.xpath(self.detail_page_xpath).extract():
